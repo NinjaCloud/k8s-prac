1880af173e92a05e6908ebc851d97a62423e98b4
Ninjacloud05 <67105537+NinjaCloud@users.noreply.github.com>
2022-11-19 12:35:50 +0530

    Update auto-completion.sh

--- auto-completion.sh
+++ auto-completion.sh
@@ -1,4 +1,9 @@
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+
 echo "alias k=kubectl" >>  ~/.bashrc_aliases
-source ~/.bashrc ~/.bashrc_aliases
+
+source ~/.bashrc
+
+source ~/.bashrc_aliases
+
 complete -F __start_kubectl k
